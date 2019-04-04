37011b15688ba0c510724d5f4048cddc2e46e93a
root <phalen.connor@hotmail.co.uk>
2019-04-04 11:49:32 -0700

    Each line of the .conf file is broken down into variables for the ip's and ports. STill an issue with sending it over properly into a new thread, as I was going to have each pairing use its own thread.

--- Code/IPPF.c
+++ Code/IPPF.c
@@ -26,10 +26,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <strings.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 #include <sys/socket.h>
 
-#define CONF_FILE "IPPF.conf" // Contains IP-Port pairs
+#define CONF_FILE "IPP_Pairs.conf" // Contains IP-Port pairs
+#define BUFLEN 512
+#define SMALLBUF 16
 
+/* ---- Function and struct Setup ---- */
+void* tprocess(void *arguments);
+void sigHandler(int sigNum);
+
+struct targs{
+	char ip1[SMALLBUF];
+	char port1[SMALLBUF];
+	char ip2[SMALLBUF];
+	char port2[SMALLBUF];
+};
 
 /* Program Start */
 int main(int argc, char **argv)
@@ -37,13 +55,91 @@ int main(int argc, char **argv)
 /* ---- Variable Setup ---- */
 	int i, maxi, nready;
 	int socket_desc, client_len; 	// Socket specific
-	int maxfd;			// Select specific
-	int clientfd[FD_SETSIZE2];
 
 	struct sockaddr_in server, client;
 
 	FILE *fr; // For reading the related .conf file
+	char rbuf[BUFLEN];
+	char *token1,  *token2;
+
+/* ---- Extract .conf File Contents ---- */
+	if((fr = fopen(CONF_FILE, "r")) == NULL) // open .conf file
+	{
+		perror("Failed to Open File");
+		exit(1);
+	}
+
+	while(fgets(rbuf, sizeof(rbuf), fr)) // read file line-by-line
+	{
+		printf("Line Characters %s\n", rbuf);
+
+		pthread_t *newthread = calloc(1, sizeof(pthread_t)); // new thread for each line
+		struct targs *args 	 = calloc(1, sizeof(struct targs)); // each thread has a few of arguments
+
+		token1 = strtok(rbuf, "-"); // IP-Port Pair #1
+		token2 = strtok(NULL, "-"); // IP-Port Pair #2
+
+		strcpy(args->ip1, strtok(token1, ":")); // get IP 1
+		strcpy(args->port1, strtok(NULL, ":")); // get Port 1
+
+		strcpy(args->ip2, strtok(token2, ":")); // get IP 2
+		strcpy(args->port2, strtok(NULL, ":")); // get Port 2
+
+		printf("%s - %s - %s - %s This Stuff \n", args->ip1, args->port1, args->ip2, args->port2);
+
+/* Uncomment when problem is figured out
+		if(pthread_create(newthread, NULL, &tprocess, (void *)&args) != 0) // make new thread to deal with each pairing
+		{
+			if(errno == EAGAIN)
+			{
+				perror("Error Occured with Thread Creation");
+				exit(1);		    		
+			}
+		}
+*/
+	}
 
 /* ---- Forwarding Setup ---- */
+	signal(SIGINT, sigHandler); // All Signal Interputs get pushed to sigintHandler
 
+	while(1)
+	{
+
+	}
+
+	fclose(fr);
+	return 0;
+}
+
+// tlisten: Thread Process for client connection and data processing
+// Input  - String containing the IP:Port pairs to forward with
+// Output - N/A
+void* tprocess(void *arguments)
+{
+	struct targs *targs = arguments; // arguments should be equal to what is printed off above, but it is getting weird
+
+	printf("%s - %s - %s - %s This Stuff \n", targs->ip1, targs->port1, targs->ip2, targs->port2);
+
+	while(1)
+	{
+
+	}
+
+	return 0;
+}
+
+// Handles Signal Interputs
+void sigHandler(int sigNum)
+{
+	switch(sigNum)
+	{
+		case SIGINT:
+			signal(SIGINT, sigHandler); // reset in case later we do more stuff 
+			printf("\nMain Program Closing...\n");
+
+			exit(0);
+			break;
+		default:
+			break;
+	}
 }
